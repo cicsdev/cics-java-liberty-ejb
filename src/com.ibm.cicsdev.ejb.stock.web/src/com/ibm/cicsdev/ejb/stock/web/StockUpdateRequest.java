ec8882f848e7b08cd2d9dec5b776ea99033c7441
Alexander Brown <brownal7@uk.ibm.com>
2017-07-28 16:40:59 +0100

    Add copyright statement.

--- src/com.ibm.cicsdev.ejb.stock.web/src/com/ibm/cicsdev/ejb/stock/web/StockUpdateRequest.java
+++ src/com.ibm.cicsdev.ejb.stock.web/src/com/ibm/cicsdev/ejb/stock/web/StockUpdateRequest.java
@@ -1,5 +1,15 @@
 package com.ibm.cicsdev.ejb.stock.web;
 
+/* Licensed Materials - Property of IBM                               */
+/*                                                                    */
+/* SAMPLE                                                             */
+/*                                                                    */
+/* (c) Copyright IBM Corp. 2017 All Rights Reserved                   */
+/*                                                                    */
+/* US Government Users Restricted Rights - Use, duplication or        */
+/* disclosure restricted by GSA ADP Schedule Contract with IBM Corp   */
+/*                                                                    */
+
 /**
  * Class representing a request to update the stock of an item.
  * 
--- src/com.ibm.cicsdev.ejb/ejbModule/com/ibm/cicsdev/ejb/CartBean.java
+++ src/com.ibm.cicsdev.ejb/ejbModule/com/ibm/cicsdev/ejb/CartBean.java
@@ -1,5 +1,15 @@
 package com.ibm.cicsdev.ejb;
 
+/* Licensed Materials - Property of IBM                               */
+/*                                                                    */
+/* SAMPLE                                                             */
+/*                                                                    */
+/* (c) Copyright IBM Corp. 2017 All Rights Reserved                   */
+/*                                                                    */
+/* US Government Users Restricted Rights - Use, duplication or        */
+/* disclosure restricted by GSA ADP Schedule Contract with IBM Corp   */
+/*                                                                    */
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
